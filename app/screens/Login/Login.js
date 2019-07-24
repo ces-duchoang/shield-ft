703844c22ca382c7d3bd1389d1ce648b12f7e22a
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-07-24 23:24:12 +0700

    add login ui

--- app/screens/Login/Login.js
+++ app/screens/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Login.scss";
-import { Layout, Form, Input, Button, Icon } from "antd";
+import ShieldIcon from "../../images/shieldicon.png";
+import { Layout, Form, Input, Button, Icon, Checkbox, Row, Avatar } from "antd";
 
 export default class Login extends React.Component {
   componentDidMount() {
@@ -8,32 +9,45 @@ export default class Login extends React.Component {
   }
   render() {
     return (
-      <Layout>
-        <Layout.Content>
-          {/* <Form layout="inline" onSubmit={this.handleSubmit}> */}
-          {/* <Form.Item> */}
-          <Input
-            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-            placeholder="Username"
-          />
-          {/* </Form.Item> */}
-          {/* <Form.Item> */}
-          <Input
-            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="password"
-            placeholder="Password"
-          />
-          {/* </Form.Item> */}
-          {/* <Form.Item> */}
-          <Button type="primary" htmlType="submit">
-            Log in
-          </Button>
-          {/* </Form.Item> */}
-          {/* </Form> */}
-        </Layout.Content>
-      </Layout>
+      <Layout.Content className="login-form-container">
+        <Row type="flex" justify="center" align="middle">
+          <Form layout="vertical" className="login-form">
+            <Row type="flex" justify="center" align="middle">
+              <Avatar size={128} src={ShieldIcon} className="login-avatar" />
+            </Row>
+            <Form.Item>
+              <Input
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                prefix={
+                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Checkbox defaultChecked={true}>Remember me</Checkbox>
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+            </Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+          </Form>
+        </Row>
+      </Layout.Content>
     );
   }
 }
-
-
