ae16d57a57ff7b49885bba4e5157b8f6dcfe3405
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-07-24 23:24:31 +0700

    add url loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,25 +8,25 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
     splitChunks: {
-      chunks: "async",
-      minSize: 30000,
-      maxSize: 0,
+      chunks: "all",
+      minSize: 10000,
+      maxSize: 250000,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: "~",
+      automaticNameDelimiter: ".",
       automaticNameMaxLength: 30,
-      name: true,
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
-          priority: -10
+          priority: 1
         },
         default: {
           minChunks: 2,
-          priority: -20,
+          priority: 0,
           reuseExistingChunk: true
-        }
+        },
+        name: "vendors"
       }
     }
   }
