b5feafe586926c7942b8d4bc216f46c9262326e0
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-07-25 23:33:20 +0700

    add validate login form

--- app/screens/Login/Login.js
+++ app/screens/Login/Login.js
@@ -2,34 +2,90 @@ import React from "react";
 import "./Login.scss";
 import ShieldIcon from "../../images/shieldicon.png";
 import { Layout, Form, Input, Button, Icon, Checkbox, Row, Avatar } from "antd";
+import Particles from "react-particles-js";
+import { validateEmail, validatePassword } from "../../validators/Account";
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      alert: {}
+    };
+  }
   componentDidMount() {
     document.title = "Đăng nhập";
   }
+  setFormState = (key, value) => {
+    this.setState({ [key]: value, alert: {} });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if (!this.isValidForm()) {
+      //TODO: call api auth
+    }
+  };
+  isValidForm = () => {
+    const email = validateEmail(this.state.email);
+    const password = validatePassword(this.state.password);
+    this.setState({ alert: { email, password } });
+    return email || password;
+  };
   render() {
     return (
       <Layout.Content className="login-form-container">
-        <Row type="flex" justify="center" align="middle">
-          <Form layout="vertical" className="login-form">
+        <Particles
+          className="ani-bg"
+          params={{
+            particles: {
+              number: {
+                value: 50
+              },
+              size: {
+                value: 3
+              }
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse"
+                }
+              }
+            }
+          }}
+        />
+        <Row type="flex" justify="center" align="middle" className="form-row">
+          <Form
+            layout="vertical"
+            className="login-form"
+            onSubmit={e => this.handleSubmit(e)}
+          >
             <Row type="flex" justify="center" align="middle">
               <Avatar size={128} src={ShieldIcon} className="login-avatar" />
             </Row>
-            <Form.Item>
+            <Form.Item {...this.state.alert.email}>
               <Input
                 prefix={
                   <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
                 }
-                placeholder="Username"
+                placeholder="Email"
+                onChange={e => {
+                  this.setFormState("email", e.target.value);
+                }}
               />
             </Form.Item>
-            <Form.Item>
+            <Form.Item {...this.state.alert.password}>
               <Input
                 prefix={
                   <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
                 }
                 type="password"
                 placeholder="Password"
+                onChange={e => {
+                  this.setFormState("password", e.target.value);
+                }}
               />
             </Form.Item>
             <Form.Item>
