124ea4cdbdffc4893f6bd882b4bac5659b832712
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-08-14 23:04:10 +0700

    add upload image api

---
+++ app/api/ImageUpload.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { getUserInfo } from "./Session";
+
+const uploadPreset = getUserInfo() ? getUserInfo().upload_preset || "" : "";
+const uploadEndpoint = `https://api.cloudinary.com/v1_1/shieldmanga/image/upload`;
+
+export default {
+  uploadImage: image => {
+    const formData = new FormData();
+    formData.append("upload_preset", uploadPreset);
+    formData.append("file", image);
+    return axios.post(uploadEndpoint, formData);
+  }
+};
