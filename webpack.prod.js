de60ae9c731ed2f68c8f4c6535fbfa1ae5a05187
DucHoang <hoangtrongminhduc@hotmail.com>
2020-02-06 10:01:15 +0700

    config remove comment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,40 +1,53 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { DefinePlugin } = require("webpack");
-require("dotenv").config();
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const {DefinePlugin} = require('webpack');
+require('dotenv').config();
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   optimization: {
-    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+    minimizer:
+    [
+      new TerserPlugin(
+          {
+            terserOptions: {
+              output: {
+                comments: false,
+              },
+            },
+            extractComments: false,
+          },
+      ),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
       minSize: 10000,
       maxSize: 250000,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: ".",
+      automaticNameDelimiter: '.',
       automaticNameMaxLength: 30,
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
-          priority: 1
+          priority: 1,
         },
         default: {
           minChunks: 2,
           priority: 0,
-          reuseExistingChunk: true
+          reuseExistingChunk: true,
         },
-        name: "vendors"
-      }
-    }
+        name: 'vendors',
+      },
+    },
   },
   plugins: [
     new DefinePlugin({
-      API_BASE_DOMAIN: JSON.stringify(process.env.API_BASE_DOMAIN)
-    })
-  ]
+      API_BASE_DOMAIN: JSON.stringify(process.env.API_BASE_DOMAIN),
+    }),
+  ],
 });
