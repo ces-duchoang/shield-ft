a6a516725f47ae40866af40f82e82c54410035bc
DucHoang <hoangtrongminhduc@hotmail.com>
2020-02-06 10:14:49 +0700

    add error overlay & custom compile log

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,56 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
-const webpack = require("webpack");
-require("dotenv").config();
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+require('dotenv').config();
 
-module.exports = env =>
+module.exports = (env) =>
   merge(common, {
-    mode: "development",
-    devtool: "source-map",
+    mode: 'development',
+    stats: 'errors-warnings',
+    devtool: 'cheap-module-source-map',
     devServer: {
       host: process.env.LOCAL_DOMAIN,
-      contentBase: "./build",
+      contentBase: './build',
       hot: true,
       open: true,
       port: env && env.PORT || process.env.PORT,
       watchContentBase: true,
-      historyApiFallback: true
+      historyApiFallback: true,
     },
     plugins: [
       new webpack.DefinePlugin({
         API_BASE_DOMAIN: JSON.stringify(
-          `${process.env.API_BASE_DOMAIN}:${process.env.BK_LOCAL_PORT}`
-        )
-      })
-    ]
+            `${process.env.API_BASE_DOMAIN}:${process.env.BK_LOCAL_PORT}`,
+        ),
+      }),
+      new CustomCompileLogs(),
+      new ErrorOverlayPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          options: {emitError: !true, emitWarning: true},
+        },
+      ]},
   });
+
+
+class CustomCompileLogs {
+  apply(compiler) {
+    compiler.hooks.beforeCompile.tapAsync('CompilingLog',
+        (params, callback) => {
+          console.clear();
+          console.log('Compiling...');
+          callback();
+        });
+    compiler.hooks.done.tap('CompiledLog',
+        (stats) => {
+          console.clear();
+          console.log('Application compiled');
+        });
+  }
+}
