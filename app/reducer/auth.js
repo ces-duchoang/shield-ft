b2ead1621ead9d47808e641349e9e637737d3557
DucHoang <hoangtrongminhduc@hotmail.com>
2020-02-15 14:03:29 +0700

    add redux initial

---
+++ app/reducer/auth.js
@@ -0,0 +1,8 @@
+const initialState = { userInfo: null, userAccesses: null };
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
